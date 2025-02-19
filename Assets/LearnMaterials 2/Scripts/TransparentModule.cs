37cfb67ea5099425df0ce53c645935f186cd77c1
maksimvorobev <maksdev505@gmail.com>
2025-02-19 19:35:39 +0700

    MaksScript

--- Assets/LearnMaterials 2/Scripts/TransparentModule.cs
+++ Assets/LearnMaterials 2/Scripts/TransparentModule.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 [HelpURL("https://docs.google.com/document/d/1Cmm__cbik5J8aHAI6PPaAUmEMF3wAcNo3rpgzsYPzDM/edit?usp=sharing")]
 public class TransparentModule : MonoBehaviour
 {
+    [SerializeField]
+    private bool Debug;
+    [SerializeField]
+    [Min(0)]
     private float changeSpeed;
 
     private float defaultAlpha;
@@ -19,6 +23,7 @@ public class TransparentModule : MonoBehaviour
         toDefault = false;
     }
 
+    [ContextMenu("Active")]
     public void ActivateModule()
     {
         float target = toDefault ? defaultAlpha : 0;
