0fd681ff36a7dd3b5a95f5591a737c4af5f85063
m0nkkke <embr4ce02@gmail.com>
2025-02-19 19:04:33 +0700

    Выполнен скрипт DestroyModule

--- Assets/LearnMaterials 2/Scripts/DestroyModule.cs
+++ Assets/LearnMaterials 2/Scripts/DestroyModule.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 [HelpURL("https://docs.google.com/document/d/1RMamVxE-yUpSfsPD_dEa4-Ak1qu6NTo83qY1O4XLxUY/edit?usp=sharing")]
 public class DestroyModule : MonoBehaviour
 {
+    [SerializeField]
+    private bool debug;
+    [SerializeField]
+    [Min(0)] 
     private float destroyDelay;
+    [SerializeField]
+    [Min(1)]
     private int minimalDestroyingObjectsCount;
 
     private Transform myTransform;
@@ -14,6 +20,7 @@ public class DestroyModule : MonoBehaviour
         myTransform = transform;
     }
 
+    [ContextMenu("Начать удаление объектов")]
     public void ActivateModule()
     {
         StartCoroutine(DestroyRandomChildObjectCoroutine());
